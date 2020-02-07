4a93a45e11596831b15152dabc5e8dd1a4b43800
André Martins <andre@basedigital.pt>
2020-02-07 23:03:12 +0000

    Add morgan middleware for dev loggin

--- routes/properties.js
+++ routes/properties.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const {
-    getProperties,
-    getProperty,
-    createProperty,
-    updateProperty,
-    deleteProperty
+  getProperties,
+  getProperty,
+  createProperty,
+  updateProperty,
+  deleteProperty
 } = require('../controllers/properties');
+
 const router = express.Router();
 
-router.route('/').get(getProperties).post(createProperty);
-router.route('/:id').get(getProperty).put(updateProperty).delete(deleteProperty);
+router
+  .route('/')
+  .get(getProperties)
+  .post(createProperty);
+router
+  .route('/:id')
+  .get(getProperty)
+  .put(updateProperty)
+  .delete(deleteProperty);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Route files
-const properties = require('./routes/properties')
+const properties = require('./routes/properties');
 
-
-
-dotenv.config({path: './config/config.env'});
-const PORT= process.env.PORT || 5000;
+dotenv.config({
+  path: './config/config.env'
+});
+const PORT = process.env.PORT || 5000;
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
 app.use('/api/v1/properties', properties);
 
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
