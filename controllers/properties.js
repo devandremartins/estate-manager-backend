d5cea454761bbaa716d1d3a350e98914e323f7c7
André Martins <andre@basedigital.pt>
2020-02-07 22:09:57 +0000

    Add property routes

---
+++ controllers/properties.js
@@ -0,0 +1,20 @@
+
+exports.getProperties = (req, res, next) => {
+    res.status(200).json({sucess: true, msg: "Show all properties"});
+}
+
+exports.getProperty = (req, res, next) => {
+    res.status(200).json({ success: true, msg:`Show property ${req.params.id}` });
+}
+
+exports.createProperty = (req, res, next) => {
+    res.status(200).json({sucess: true, msg: "Create property"});
+}
+
+exports.updateProperty = (req, res, next) => {
+    res.status(200).json({ success: true, msg:`Show property ${req.params.id}` });
+}
+
+exports.deleteProperty = (req, res, next) => {
+    res.status(200).json({ success: true, msg:`Show property ${req.params.id}` });    
+}
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const properties = require('./routes/properties')
+
+
+
 dotenv.config({path: './config/config.env'});
 const PORT= process.env.PORT || 5000;
-
 const app = express();
 
-app.get('/api/v1/properties', (req, res) => {
-    res.status(200).json({sucess: true, msg: "Show all properties"});
-})
+app.use('/api/v1/properties', properties);
+
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
