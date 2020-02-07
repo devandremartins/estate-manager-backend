e455878d37ed7e2b44603312f1663a617c91474b
André Martins <andre@basedigital.pt>
2020-02-07 21:33:55 +0000

    Add first routes

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ dotenv.config({path: './config/config.env'});
 const PORT= process.env.PORT || 5000;
 
 const app = express();
+
+app.get('/api/v1/properties', (req, res) => {
+    res.status(200).json({sucess: true, msg: "Show all properties"});
+})
+
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
