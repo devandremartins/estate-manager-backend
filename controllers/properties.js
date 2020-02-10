cc7f3142327d065917b5748f310dcb7004ff48ff
André Martins <andre@basedigital.pt>
2020-02-10 21:56:40 +0000

    Properties pagination and limits

--- controllers/properties.js
+++ controllers/properties.js
@@ -9,7 +9,7 @@ exports.getProperties = asyncHandler(async (req, res, next) => {
     ...req.query
   };
   // Fields to exlude
-  const removeFields = ['select'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
   // Loop fields and remove them from query
   removeFields.forEach(param => delete reqQuery[param]);
 
@@ -27,13 +27,46 @@ exports.getProperties = asyncHandler(async (req, res, next) => {
     const fields = req.query.select.split(',').join(' ');
     query = query.select(fields);
   }
+  // Sort
+  if (req.param.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Property.countDocuments();
+  query = query.skip(startIndex).limit(limit);
 
   // Execute query
   const properties = await query;
 
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
+
   res.status(200).json({
     success: true,
     count: properties.length,
+    pagination,
     data: properties
   });
 });
