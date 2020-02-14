230665b7a0fa1dab6c60121e320cbeb89cbf916c
André Martins <andre@basedigital.pt>
2020-02-14 15:18:22 +0000

    Login is now working and returning a token

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,3 +24,36 @@ exports.register = asyncHandler(async (req, res, next) => {
     token
   });
 });
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const {
+    email,
+    password
+  } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for the user
+  const user = await User.findOne({
+    email
+  }).select('+password');
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token
+  });
+});
